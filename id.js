6f9c1e90f1331bbfc0c8be0b04c22df09c32aaaa
nabeel0019 <masoominsaan12@gmail.com>
2025-11-01 00:50:27 +0500

    Add files via upload

---
+++ id.js
@@ -0,0 +1,10 @@
+Function makeid(num = 4){
+  let result = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyt0123456789";
+  var characters9 = characters.length;
+  for (var i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters9));
+  }
+  return result;
+}
+module.exports = {makeid};
